317cf8f67eec222876d876b44bfdc145236f22a0
Pablo Marco <pmarcomunoz@gmail.com>
2023-05-28 23:47:14 +0200

    T04 -TipoCita delete -Seguimiento delete

--- src/main/java/com/inmozara/crm/seguimiento/model/Seguimiento.java
+++ src/main/java/com/inmozara/crm/seguimiento/model/Seguimiento.java
@@ -3,12 +3,14 @@ package com.inmozara.crm.seguimiento.model;
 import com.inmozara.crm.inmueble.model.Inmueble;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Entity(name = "seguimientos")
 public class Seguimiento {
     @Id
