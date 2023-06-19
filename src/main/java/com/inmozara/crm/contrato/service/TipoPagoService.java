a20506130c7da611822ebf3ad4e5ae460f948399
Pablo Marco <pmarcomunoz@gmail.com>
2023-06-19 19:45:26 +0200

    T05 crud tipo pago

--- src/main/java/com/inmozara/crm/contrato/service/TipoPagoService.java
+++ src/main/java/com/inmozara/crm/contrato/service/TipoPagoService.java
@@ -1,12 +1,10 @@
 package com.inmozara.crm.contrato.service;
 
 import com.inmozara.crm.config.MensajeDTO;
-import com.inmozara.crm.contrato.model.Contrato;
 import com.inmozara.crm.contrato.model.TipoPago;
 import com.inmozara.crm.contrato.model.dto.TipoPagoDTO;
 import com.inmozara.crm.contrato.model.repository.ContratoRepository;
 import com.inmozara.crm.contrato.model.repository.TipoPagoRepository;
-import com.inmozara.crm.contrato.model.search.ContratoSearch;
 import com.inmozara.crm.contrato.service.interfaces.ITipoPago;
 import com.inmozara.crm.excepcion.RecursoNoEncontrado;
 import com.inmozara.crm.utils.ObjectMapperUtils;
@@ -16,6 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TipoPagoService implements ITipoPago {
@@ -43,14 +42,7 @@ public class TipoPagoService implements ITipoPago {
         if (!tipoPagoRepository.existsById(idTipoPago)) {
             throw new RecursoNoEncontrado("El tipo de pago no existe");
         }
-        List<Contrato> contratos = contratoRepository.findAll(ContratoSearch.builder()
-                .contrato(Contrato.builder().tipoPago(TipoPago.builder().idTipoPago(idTipoPago).build()).build()).build());
-        if (!contratos.isEmpty()) {
-            contratos.forEach(contrato -> {
-                contrato.setTipoPago(TipoPago.builder().idTipoPago(0L).build());
-                contratoRepository.save(contrato);
-            });
-        }
+        contratoRepository.actualizarContratosPorTipoPago(TipoPago.builder().idTipoPago(idTipoPago).build(), TipoPago.builder().idTipoPago(0L).build());
         tipoPagoRepository.deleteById(idTipoPago);
         return MensajeDTO.builder().mensaje("El tipo de pago se ha eliminado correctamente con el id: " + idTipoPago).estado(HttpStatus.OK.value()).fecha(UtilsDates.now()).build();
     }
@@ -67,6 +59,10 @@ public class TipoPagoService implements ITipoPago {
         if (tipoPagos.isEmpty()) {
             throw new RecursoNoEncontrado("No hay tipos de pagos");
         }
+        tipoPagos = tipoPagos
+                .stream()
+                .filter(tipoPago -> tipoPago.getIdTipoPago() != 0L)
+                .collect(Collectors.toList());
         return ObjectMapperUtils.mapAll(tipoPagos, TipoPagoDTO.class);
     }
 }
