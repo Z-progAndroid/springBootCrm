1a297c3bea912d88d57bd43ac57dd014b8e2e38a
Pablo Marco <pmarcomunoz@gmail.com>
2023-08-19 20:21:12 +0200

    T11 borrado seguimiento y tipo seguimiento

--- src/main/java/com/inmozara/crm/seguimiento/service/interfaces/ISeguimiento.java
+++
@@ -1,7 +0,0 @@
-package com.inmozara.crm.seguimiento.service.interfaces;
-
-import com.inmozara.crm.seguimiento.model.dto.SeguimientoDTO;
-import com.inmozara.crm.utils.comon.IService;
-
-public interface ISeguimiento extends IService<SeguimientoDTO,Long> {
-}
--- src/main/java/com/inmozara/crm/seguimiento/service/interfaces/ITipoSeguimiento.java
+++
@@ -1,7 +0,0 @@
-package com.inmozara.crm.seguimiento.service.interfaces;
-
-import com.inmozara.crm.seguimiento.model.dto.TipoSeguimientoDTO;
-import com.inmozara.crm.utils.comon.IService;
-
-public interface ITipoSeguimiento extends IService<TipoSeguimientoDTO,Long> {
-}
