17e10ce49b27140864a0e86fa6516415200ad075
Pablo Marco <pmarcomunoz@gmail.com>
2023-08-31 19:38:01 +0200

    T15-data.sql

--- src/main/resources/static/sql/provincias.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO crm.provincias
-(id_provincia, fecha_creacion, fecha_modificacion, modificado, provincia, id_pais)
-VALUES(50, CURRENT_DATE(), CURRENT_DATE(), 'CREACION_INICIAL', 'ZARAGOZA', 'ES');
